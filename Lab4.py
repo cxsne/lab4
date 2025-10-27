51f08c7afcd051c7af00fed553462571f596697b
Sam Brown <sbrown@plu.edu>
2025-10-26 21:51:43 -0700

    Implemented DTPredict.py

--- Lab4.py
+++ Lab4.py
@@ -204,48 +204,50 @@ def write_node(outfile,curr):
     outfile.write(" ) ")
 
 def read_model(modelfile):
+    global root
+
     with open(modelfile, 'r') as f:
             attline = f.readline().strip()
             attarr = attline.split()
             tokens = f.read().replace('(', ' ( ').replace(')', ' ) ').split()
 
-        root = None
-        stack = []
-        current_node = None
-        index = 0
-        while index < len(tokens):
-            token = tokens[index]
+    root = None
+    stack = []
+    current_node = None
+    index = 0
+    while index < len(tokens):
+        token = tokens[index]
 
-            if token.startwith('['):
-                leaf = Treenode(None)
-                leaf.returnval = token[1:-1]
+        if token.startswith('['):
+            leaf = Treenode(None)
+            leaf.returnval = token[1:-1]
 
-                if stack:
-                    parent, key = stack.pop()
-                    leaf.parent = parent
-                    parent.children[key] = leaf
-                else:
-                    root = leaf
-                index += 1
-            elif token == '(':
-                index += 1
-            elif token == ')':
-                current_node = None if not stack else stack[-1][0]
+            if stack:
+                parent, key = stack.pop()
+                leaf.parent = parent
+                parent.children[key] = leaf
+            else:
+                root = leaf
+            index += 1
+        elif token == '(':
+            index += 1
+        elif token == ')':
+            current_node = None if not stack else stack[-1][0]
+            index += 1
+        else:
+            if current_node is None or current_node.returnval is not None:
+                node = Treenode(None)
+                node.attribute = token
+
+                if not root:
+                    root = node
+                current_node = node
                 index += 1
             else:
-                if current_node is None or current_node.returnval is not None:
-                    node = Treenode(None)
-                    node.attribute = token
+                stack.append((current_node, token))
+                index += 1
 
-                    if not root:
-                        root = node
-                    current_node = node
-                    index += 1
-                else:
-                    stack.append((current_node, token))
-                    index += 1
-
-        return root, attarr
+    return root, attarr
 
 
 def DTtrain(data, model):
@@ -267,9 +269,9 @@ def trace_tree(node, data, attarr):
             else:
                 val = data[index]
                 node = node.children.get(val, list(node.children.values())[0])
-            except ValueError:
-                node = list(node.children.values())[0]
-        return node.returnval
+        except ValueError:
+            node = list(node.children.values())[0]
+    return node.returnval
     
 def DTpredict(data, model, prediction):
     """
@@ -284,19 +286,19 @@ def DTpredict(data, model, prediction):
     """
     # implement your code here
     global root, atts
-        root, atts = read_model(model)
+    root, atts = read_model(model)
 
-        predictions = []
-        with open(data, 'r') as f:
-            for line in f:
-                tokens = line.strip().split()
-                datapoint = tokens[1:]
-                pred = trace_tree(root, datapoint,atts)
-                predictions.append(pred)
+    predictions = []
+    with open(data, 'r') as f:
+        for line in f:
+            tokens = line.strip().split()
+            datapoint = tokens[1:]
+            pred = trace_tree(root, datapoint,atts)
+            predictions.append(pred)
 
-        with open(prediction, 'w') as f:
-            for pred in predictions:
-                f.write(pred + '\n')
+    with open(prediction, 'w') as f:
+        for pred in predictions:
+            f.write(pred + '\n')
 
 
 def EvaDT(predictionLabel, realLabel, output):
